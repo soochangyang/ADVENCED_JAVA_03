5e794ab088506dc296b2125438b5478a57a0c65a
yang <soochang.yang@gmail.com>
2026-06-05 15:50:50 +0900

    lambda Supplier

---
+++ src/lambda/lambda4/SupplierMain.java
@@ -0,0 +1,22 @@
+package lambda.lambda4;
+
+import java.util.function.Supplier;
+
+public class SupplierMain {
+
+    static void main(String[] args) {
+        //Anonymous Class
+        Supplier<String> supplier1 = new Supplier<String>() {
+            @Override
+            public String get() {
+                return "Welcome to supplier Anonymous class";
+            }
+        };
+
+        System.out.println(supplier1.get());
+
+        //Lambda.
+        Supplier<String> supplier2 = () -> "Welcome to lambda supplier";
+        System.out.println(supplier2.get());
+    }
+}
